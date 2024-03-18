cb87719353b630d289fa300100dc6b8b9ba283f6
Hong Yi <im@hongyi.io>
2024-03-18 19:06:16 +0800

    fix desc

--- Formula/poui.rb
+++ Formula/poui.rb
@@ -1,5 +1,5 @@
 class Poui < Formula
-  desc "Please Open UI (POUI) - An application to help you open your relevant developer portals"
+  desc "An application to help you open your relevant developer portals"
   homepage "https://github.com/hong-yi/poui"
   url "https://github.com/hong-yi/poui/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "7d29489551a8bf43b52a5084d28b16adc5d75d0bc61587a10e72fabe146c8fc3"
